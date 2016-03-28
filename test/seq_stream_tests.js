69addb22ec88919f19698b26ffc4c2a3cb4b8afa
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-03-28 15:04:06 +1000

    Add a test so that the build's happy.

---
+++ test/seq_stream_tests.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let SeqStream = require('../seq_stream');
+
+describe('SeqStream', () => {
+   describe('constructor', () => {
+       it('can be constructed', () => {
+          new SeqStream(); 
+       });
+   });
+});
