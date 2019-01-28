6fbf2394f759511df321c1d44aeac7a48b37d668
Nathan Brown <nathan@nkbrown.us>
2019-01-27 17:51:26 -0700

    Add flushToBeacon for browsers to have a chance to send data when navigating away.

--- seq_stream.js
+++ seq_stream.js
@@ -61,6 +61,15 @@ class SeqStream extends stream.Writable {
     flush() {
         return this._logger.flush();
     }
+
+    // A browser only function that queues events for sending using the
+    // navigator.sendBeacon() API.  This may work in an unload or pagehide event
+    // handler when a normal flush() would not.
+    // Events over 63K in length are discarded (with a warning sent in its place) 
+    // and the total size batch will be no more than 63K in length.
+    flushToBeacon() {
+        return this._logger.flushToBeacon();
+    }
 }
 
 module.exports = SeqStream;
