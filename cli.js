36d37b635befb6a496cc19c507e865d0b8cea5dc
larenelg <lotusgenome@gmail.com>
2020-09-18 10:38:37 +1000

    can add extra properties to bunyan-seq cli events

--- cli.js
+++ cli.js
@@ -23,9 +23,21 @@ function main() {
         return undefined;
       }
     )
-    .action(({ serverUrl, apiKey, logOtherAs }) => {
+    .option('-p, --property <property>', 'Properties to add to all logs', (value, previous) => {
+      const valueSplit = value.split('=')
+      if(valueSplit.length !== 2) {
+        console.error(`Warning, skipping option "property": Invalid value specified: "${value}`)
+        return previous;
+      }
+
+      const current = {
+        [valueSplit[0]]: valueSplit[1]
+      }
+      return Object.assign(previous, current);
+    }, {})
+    .action(({ serverUrl, apiKey, logOtherAs, property }) => {
       try {
-        const seqStream = new SeqStream({ serverUrl, apiKey });
+        const seqStream = new SeqStream({ serverUrl, apiKey, additionalProperties: property });
 
         process.stdin.pipe(split2()).pipe(new StringStream({ logOtherAs })).pipe(seqStream).on("error", console.error);
 
